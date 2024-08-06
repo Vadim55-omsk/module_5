700ee3ef45131e0d098e7f849a42d07d9509437f
Vadim55-omsk <fvg-income@yandex.ru>
2024-08-06 16:42:07 +0300

    Update module_5.3_Перегрузка операторов.py

--- module_5.3_Перегрузка операторов.py
+++ module_5.3_Перегрузка операторов.py
@@ -25,12 +25,7 @@ class House:
     def __add__(self, value):
         self.number_of_floors += value
         return self
-
-    # def __add__(self,value):
-    #     if isinstance(value, int):
-    #         self.number_of_floors = self.number_of_floors + value
-    #         return self
-
+   
     def __radd__(self, value):
         self.number_of_floors += value
         return self
@@ -38,7 +33,6 @@ class House:
     def __iadd__(self, value):
         self.number_of_floors += value
         return self
-
 
 
 h1 = House("ЖК Эльбрус", number_of_floors = 18)
